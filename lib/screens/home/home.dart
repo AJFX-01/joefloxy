a6f70b4584391a06b904da23aabda51a4e56712d
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-12-15 04:17:00 +0100

    feat: starting the home page

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constant_utils.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.whiteColor,
+      appBar: ,
+      body: ,
+    );
+  }
+}
