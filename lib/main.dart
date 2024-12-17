1e15991a60f233a4b13e4c8447abf6fd55effd9e
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-12-17 18:23:30 +0100

    feat: about to start the tab section

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-      initialRoute: Routes.homePage,
+      initialRoute: Routes.splashPage,
       routes: Routes.pages,
     );
   }
---
+++ lib/screens/home/components/tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabSection extends StatefulWidget {
+  const TabSection({super.key});
+
+  @override
+  State<TabSection> createState() => _TabSectionState();
+}
+
+class _TabSectionState extends State<TabSection> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
